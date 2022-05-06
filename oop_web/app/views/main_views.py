c6136bffcddb85a580423e2e9466f9122bbb0744
Seongho An <88428287+SeonghoAn@users.noreply.github.com>
2022-05-06 17:53:49 +0900

    Update main_views.py

--- oop_web/app/views/main_views.py
+++ oop_web/app/views/main_views.py
@@ -3,13 +3,6 @@ from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-
-@bp.route('/unittest')
-def test():
-    return 'Hello!'
-
 @bp.route('/')
 def index():
     return redirect(url_for('article._list'))
-
-
